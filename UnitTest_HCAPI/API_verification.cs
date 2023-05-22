d230e39f0f160881a044f748db0d71ed5e1b17da
Péter Szalay <peter.szalay@stud.uni-corvinus.hu>
2023-05-23 00:21:50 +0200

    UNIT TEST

---
+++ UnitTest_HCAPI/API_verification.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Hotcakes.Commerce.Catalog;
+using Hotcakes.Commerce.Dnn.Prompt;
+using Hotcakes.CommerceDTO.v1.Client;
+
+namespace UnitTest_HCAPI
+{
+    internal class API_verification
+    {
+        private Api api;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize the API client
+            api = new Api("http://20.234.113.211:8083", "1-6bd2d3e3-d6ff-4d43-80de-4e1efab85207");
+        }
+
+        [Test]
+        public void TestProductsFindAll()
+        {
+   
+            var result = api.ProductsFindAll();
+     
+            Assert.NotNull(result);
+            Assert.IsNotEmpty(result.Content);
+
+        }
+    }
+}
---
+++ UnitTest_HCAPI/ProductsUpdate.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotcakes.Commerce.Catalog;
+using Hotcakes.Commerce.Dnn.Prompt;
+using Hotcakes.CommerceDTO.v1.Client;
+
+
+namespace UnitTest_HCAPI
+{
+    internal class ProductsUpdate
+    {
+        [Test]
+        
+        public void TestProductsUpdate()
+        {
+            // Arrange
+            var proxy = new Api("http://20.234.113.211:8083", "1-6bd2d3e3-d6ff-4d43-80de-4e1efab85207");
+            var products = proxy.ProductsFindAll();
+            var originalPrice = products.Content[0].SitePrice;
+
+            // Act
+
+            var selectedProduct = products.Content.FirstOrDefault(p => p.ProductName == "Test Product 1");
+
+            if (selectedProduct != null)
+            {
+                selectedProduct.SitePrice += 10;
+
+                var updateResult = proxy.ProductsUpdate(selectedProduct);
+
+                // Assert
+                Assert.IsEmpty(updateResult.Errors);
+                Assert.AreEqual(originalPrice + 10, selectedProduct.SitePrice);
+
+                // Set the price back to the original value
+                selectedProduct.SitePrice = originalPrice;
+                proxy.ProductsUpdate(selectedProduct);
+            }
+            
+        }
+
+
+
+
+
+    }
+}
