6f51c52bc51aa76adcde8fe2b1279e7d7f8231ac
Péter Szalay <peter.szalay@stud.uni-corvinus.hu>
2023-05-23 00:25:47 +0200

    HC_API

---
+++ TEST2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TEST2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.txtNewPrice = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.Location = new System.Drawing.Point(12, 63);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(234, 433);
+            this.lstProducts.TabIndex = 0;
+            // 
+            // txtNewPrice
+            // 
+            this.txtNewPrice.Location = new System.Drawing.Point(544, 341);
+            this.txtNewPrice.Name = "txtNewPrice";
+            this.txtNewPrice.Size = new System.Drawing.Size(196, 20);
+            this.txtNewPrice.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(544, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(742, 430);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(130, 57);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current price of the selected product:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Write here the new price:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select the product:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNewPrice);
+            this.Controls.Add(this.lstProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.TextBox txtNewPrice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TEST2/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Proxies;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.Commerce.Catalog;
+using Hotcakes.Commerce.Dnn.Prompt;
+using Hotcakes.CommerceDTO.v1.Client;
+//using Hotcakes.CommerceDTO.v1.Catalog;
+//using Hotcakes.CommerceDTO.v1.Contacts;
+
+
+namespace TEST2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var proxy = new Api("http://20.234.113.211:8083", "1-6bd2d3e3-d6ff-4d43-80de-4e1efab85207");
+
+            var products = proxy.ProductsFindAll();
+
+            foreach (var product in products.Content)
+            {
+                lstProducts.Items.Add(product.ProductName);
+            }
+            lstProducts.SelectedIndexChanged += (sender, e) =>
+            {
+                var selectedProduct = products.Content.FirstOrDefault(p => p.ProductName == lstProducts.SelectedItem.ToString());
+
+                if (selectedProduct != null)
+                {
+                    textBox1.Text = selectedProduct.SitePrice.ToString();
+                }
+            };
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            var proxy = new Api("http://20.234.113.211:8083", "1-6bd2d3e3-d6ff-4d43-80de-4e1efab85207");
+            var products = proxy.ProductsFindAll();
+
+            if (!string.IsNullOrWhiteSpace(txtNewPrice.Text))
+            {
+
+                if (Regex.IsMatch(txtNewPrice.Text, @"^\d+(\.\d{1,2})?$"))
+                {
+                    if (decimal.TryParse(txtNewPrice.Text, out decimal newPrice))
+                    {
+                        var selectedProduct = products.Content.FirstOrDefault(p => p.ProductName == lstProducts.SelectedItem.ToString());
+                        selectedProduct.SitePrice = newPrice;
+
+                        var updateResult = proxy.ProductsUpdate(selectedProduct);
+
+                        txtNewPrice.Text = "";
+
+                        if (updateResult.Errors.Count > 0)
+                        {
+                            // Handle any errors that occurred
+                            MessageBox.Show("Error updating product: " + updateResult.Errors[0].Description);
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Price of {selectedProduct.ProductName} updated successfully to {selectedProduct.SitePrice} $");
+                            textBox1.Text = selectedProduct.SitePrice.ToString(); // Update the text box with the new price
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid price entered");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid price format. Please enter a valid price (e.g. 25.99)");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a price before updating");
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
